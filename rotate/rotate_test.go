da320ded44d1498b0d8d35482b9a78fedeaf8458
Gastón Simone <gaston.simone@gmail.com>
2016-02-05 08:36:06 -0300

    Improve rotate tests.

--- rotate/rotate_test.go
+++ rotate/rotate_test.go
@@ -18,93 +18,130 @@ func areEqual(s, t []string) bool {
 	return true
 }
 
-func TestReverse(t *testing.T) {
+func TestReverseEmpty(t *testing.T) {
 	var a []string = nil
 
 	Reverse(a)
+
 	var want []string = nil
-	if !areEqual(a, want) {
-		t.Fatalf("got %v, want %v", a, want)
-	}
-
-	a = []string{"1"}
-
-	Reverse(a)
-	want = []string{"1"}
-	if !areEqual(a, want) {
-		t.Fatalf("got %v, want %v", a, want)
-	}
-
-	a = []string{"1", "2", "3", "4"}
-
-	Reverse(a)
-	want = []string{"4", "3", "2", "1"}
-	if !areEqual(a, want) {
-		t.Fatalf("got %v, want %v", a, want)
-	}
-
-	a = []string{"1", "2", "3", "4", "5"}
-
-	Reverse(a)
-	want = []string{"5", "4", "3", "2", "1"}
 	if !areEqual(a, want) {
 		t.Fatalf("got %v, want %v", a, want)
 	}
 }
 
-func TestRotate(t *testing.T) {
+func TestReverseSingleItem(t *testing.T) {
+	a := []string{"1"}
+
+	Reverse(a)
+
+	want := []string{"1"}
+	if !areEqual(a, want) {
+		t.Fatalf("got %v, want %v", a, want)
+	}
+}
+
+func TestReverseEven(t *testing.T) {
+	a := []string{"1", "2", "3", "4"}
+
+	Reverse(a)
+	want := []string{"4", "3", "2", "1"}
+	if !areEqual(a, want) {
+		t.Fatalf("got %v, want %v", a, want)
+	}
+}
+
+func TestReverseOdd(t *testing.T) {
+	a := []string{"1", "2", "3", "4", "5"}
+
+	Reverse(a)
+	want := []string{"5", "4", "3", "2", "1"}
+	if !areEqual(a, want) {
+		t.Fatalf("got %v, want %v", a, want)
+	}
+}
+
+func TestRotateEmpty(t *testing.T) {
 	err := Rotate(nil, 1)
 	if err != nil {
 		t.Fatalf("unexpected error: %v", err)
 	}
+}
 
+func TestRotateLeft(t *testing.T) {
 	a := []string{"1", "2", "3", "4", "5", "6", "7"}
 
-	err = Rotate(a, -1)
+	err := Rotate(a, -1)
 	if err == nil {
 		t.Fatal("Expected error")
 	}
+}
 
-	err = Rotate(a, 0)
+func TestRotateNothing(t *testing.T) {
+	a := []string{"1", "2", "3", "4", "5", "6", "7"}
+
+	err := Rotate(a, 0)
 	if err != nil {
 		t.Fatalf("unexpected error: %v", err)
 	}
-	want1 := []string{"1", "2", "3", "4", "5", "6", "7"}
-	if !areEqual(a, want1) {
-		t.Fatalf("got %v, want %v", a, want1)
-	}
 
-	err = Rotate(a, 1)
+	want := []string{"1", "2", "3", "4", "5", "6", "7"}
+	if !areEqual(a, want) {
+		t.Fatalf("got %v, want %v", a, want)
+	}
+}
+
+func TestRotateOne(t *testing.T) {
+	a := []string{"1", "2", "3", "4", "5", "6", "7"}
+
+	err := Rotate(a, 1)
 	if err != nil {
 		t.Fatalf("unexpected error: %v", err)
 	}
-	want2 := []string{"7", "1", "2", "3", "4", "5", "6"}
-	if !areEqual(a, want2) {
-		t.Fatalf("got %v, want %v", a, want2)
-	}
 
-	err = Rotate(a, 6)
+	want := []string{"7", "1", "2", "3", "4", "5", "6"}
+	if !areEqual(a, want) {
+		t.Fatalf("got %v, want %v", a, want)
+	}
+}
+
+func TestRotateAllButOne(t *testing.T) {
+	a := []string{"7", "1", "2", "3", "4", "5", "6"}
+
+	err := Rotate(a, 6)
 	if err != nil {
 		t.Fatalf("unexpected error: %v", err)
 	}
-	if !areEqual(a, want1) {
-		t.Fatalf("got %v, want %v", a, want1)
-	}
 
-	err = Rotate(a, 7)
+	want := []string{"1", "2", "3", "4", "5", "6", "7"}
+	if !areEqual(a, want) {
+		t.Fatalf("got %v want %v", a, want)
+	}
+}
+
+func TestRotateEverything(t *testing.T) {
+	a := []string{"1", "2", "3", "4", "5", "6", "7"}
+
+	err := Rotate(a, 7)
 	if err != nil {
 		t.Fatalf("unexpected error: %v", err)
 	}
-	if !areEqual(a, want1) {
-		t.Fatalf("got %v, want %v", a, want1)
-	}
 
-	err = Rotate(a, 10)
+	want := []string{"1", "2", "3", "4", "5", "6", "7"}
+	if !areEqual(a, want) {
+		t.Fatalf("got %v, want %v", a, want)
+	}
+}
+
+func TestRotateMoreThanLength(t *testing.T) {
+	a := []string{"1", "2", "3", "4", "5", "6", "7"}
+
+	err := Rotate(a, 10)
 	if err != nil {
 		t.Fatalf("unexpected error: %v", err)
 	}
-	want3 := []string{"5", "6", "7", "1", "2", "3", "4"}
-	if !areEqual(a, want3) {
-		t.Fatalf("got %v, want %v", a, want3)
+
+	want := []string{"5", "6", "7", "1", "2", "3", "4"}
+	if !areEqual(a, want) {
+		t.Fatalf("got %v, want %v", a, want)
 	}
 }
